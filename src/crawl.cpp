0b215b2671d894a74c323adc917822de0bb3bce9
Claus Kovacs <claus@kovacs.cf>
2021-03-31 15:31:22 +0100

    URL information extraction: curriculum type (BSc, MSc, Phd, etc.)

--- src/crawl.cpp
+++ src/crawl.cpp
@@ -75,7 +75,10 @@ void fetch_PDF_from_URL(std::string fetch_pdf_url)
 	}
 }
 
-void testtt(std::string result, std::vector<std::string>& extracted_data1, std::vector<std::string>& extracted_data2)
+
+// starting from the obtained page source code, this function extracts the links to the PDFs as well as the information about the PDFs. The obtained
+// information is stored in the two std::vectors extracted_data1 and extracted_data2
+void extract_PDF_URL_and_descriptions(std::string result, std::vector<std::string>& fetched_URLs, std::vector<std::string>& fetched_URLs_descriptions)
 {
 	// extract the links and descriptions of the PDFs
 	int shift_pos = 0;
@@ -104,7 +107,7 @@ void testtt(std::string result, std::vector<std::string>& extracted_data1, std::
 				{
 					close_URL = true;
 					extract_URL.append(result.begin()+start_extract_pos_URL, result.begin()+i);
-					extracted_data1.push_back(extract_URL);
+					fetched_URLs.push_back(extract_URL);
 				}
 				else if (i == result.length())	// error .. end of string while tag is open!
 				{
@@ -133,7 +136,7 @@ void testtt(std::string result, std::vector<std::string>& extracted_data1, std::
 				{
 					close_descr = true;
 					extract_desc.append(result.begin()+start_extract_pos_desc, result.begin()+i);
-					extracted_data2.push_back(extract_desc);
+					fetched_URLs_descriptions.push_back(extract_desc);
 				}
 				else if (i == result.length())	// error .. end of string while tag is open!
 				{
@@ -144,27 +147,27 @@ void testtt(std::string result, std::vector<std::string>& extracted_data1, std::
 			}
 		}
 	}
-
 }
 
 
 int main()
 {
 
+
+
+std::string result = "<p>UE 066 443 <a href=\"/fileadmin/Assets/dienstleister/studienabteilung/MSc_Studienplaene_2020/Masterstudium_Architektur_2020.pdf\" target=\"_blank\">Architektur<span class=\"sr-only\">, öffnet eine Datei in einem neuen Fenster</span></a></p><p>UE 066 444 <a href=\"/fileadmin/Assets/dienstleister/studienabteilung/Alte_Studienplaene/Masterstudium_Building_Science_and_Technology.pdf\" target=\"_blank\">Building Science &amp; Technologie<span class=\"sr-only\">, öffnet eine Datei in einem neuen Fenster</span></a> englischsprachig</p><h3>Bauingenieurwesen</h3><p>UE 066 505 <a href=\"/fileadmin/Assets/dienstleister/studienabteilung/MSc_Studienplaene_2020/Masterstudium_Bauingenieurwissenschaften_2020.pdf\" target=\"_blank\">Bauingenieurwissenschaften <span class=\"sr-only\">, öffnet eine Datei in einem neuen Fenster</span></a></p><h3>Biomedical Engineering (interfakultär)</h3><p>UE 066 453 <a href=\"/fileadmin/Assets/dienstleister/studienabteilung/MSc_Studienplaene_2020/Masterstudium_Biomedical_Engineering_2020.pdf\" target=\"_blank\">Biomedical Engineering<span class=\"sr-only\">, öffnet eine Datei in einem neuen Fenster</span></a> (englischsprachig)</p><h3>Computational Science and Engineering (interfakultär)</h3><p>UE 066 646 <a href=\"/fileadmin/Assets/dienstleister/studienabteilung/MSc_Studienplaene_2020/Masterstudium_Computational_Science_and_Engineering_2020.pdf\" target=\"_blank\">Computational Science and Engineering<span class=\"sr-only\">, öffnet eine Datei in einem neuen Fenster</span></a></p><h3>Elektrotechnik</h3><p>UE 066 504 <a href=\"/fileadmin/Assets/dienstleister/studienabteilung/MSc_Studienplaene_2020/Masterstudium_Embedded_Systems_2020.pdf\" target=\"_blank\">Embedded Systems<span class=\"sr-only\">, öffnet eine Datei in einem neuen Fenster</span></a></p><p>UE 066 506 <a href=\"/fileadmin/Assets/dienstleister/studienabteilung/MSc_Studienplaene_2018/MasterEnergieundAutomatisierungstechnik.pdf\" target=\"_blank\">Energie- und Automatiesierungstechnik<span class=\"sr-only\">, öffnet eine Datei in einem neuen Fenster</span></a></p><p>UE 066 507 <a href=\"/fileadmin/Assets/dienstleister/studienabteilung/MSc_Studienplaene_2019/Masterstudium_Telecommunications.pdf\" target=\"_blank\">Telecommunications <span class=\"sr-only\">, öffnet eine Datei in einem neuen Fenster</span></a>englischsprachig</p><p>UE 066 508 ";
+
+
+
+
+
+
+
+
+
+
+
 	// 1. fetch the source code of the page
-
-	// 2. extract the paths to the PDFs
-
-
-	// 3. download (all) the PDFs
-
-
-	// 4. compare PDFs (new one?, new version?, changed version? -> hash the file -> save file if it is a new one)
-
-
-	// 5. create a lof of the crawl
-
-
 
 	// the site one wants to crawl
 //	std::string page_to_crawl = "https://www.tuwien.at/tu-wien/organisation/zentrale-bereiche/studienabteilung/studienplaene";
@@ -172,22 +175,47 @@ int main()
 	// fetch the source code of the given page
 //	std::string result = fetch_source_single_page(page_to_crawl);
 
-std::string result = "<p>UE 066 443 <a href=\"/fileadmin/Assets/dienstleister/studienabteilung/MSc_Studienplaene_2020/Masterstudium_Architektur_2020.pdf\" target=\"_blank\">Architektur<span class=\"sr-only\">, öffnet eine Datei in einem neuen Fenster</span></a></p><p>UE 066 444 <a href=\"/fileadmin/Assets/dienstleister/studienabteilung/Alte_Studienplaene/Masterstudium_Building_Science_and_Technology.pdf\" target=\"_blank\">Building Science &amp; Technologie<span class=\"sr-only\">, öffnet eine Datei in einem neuen Fenster</span></a> englischsprachig</p><h3>Bauingenieurwesen</h3><p>UE 066 505 <a href=\"/fileadmin/Assets/dienstleister/studienabteilung/MSc_Studienplaene_2020/Masterstudium_Bauingenieurwissenschaften_2020.pdf\" target=\"_blank\">Bauingenieurwissenschaften <span class=\"sr-only\">, öffnet eine Datei in einem neuen Fenster</span></a></p><h3>Biomedical Engineering (interfakultär)</h3><p>UE 066 453 <a href=\"/fileadmin/Assets/dienstleister/studienabteilung/MSc_Studienplaene_2020/Masterstudium_Biomedical_Engineering_2020.pdf\" target=\"_blank\">Biomedical Engineering<span class=\"sr-only\">, öffnet eine Datei in einem neuen Fenster</span></a> (englischsprachig)</p><h3>Computational Science and Engineering (interfakultär)</h3><p>UE 066 646 <a href=\"/fileadmin/Assets/dienstleister/studienabteilung/MSc_Studienplaene_2020/Masterstudium_Computational_Science_and_Engineering_2020.pdf\" target=\"_blank\">Computational Science and Engineering<span class=\"sr-only\">, öffnet eine Datei in einem neuen Fenster</span></a></p><h3>Elektrotechnik</h3><p>UE 066 504 <a href=\"/fileadmin/Assets/dienstleister/studienabteilung/MSc_Studienplaene_2020/Masterstudium_Embedded_Systems_2020.pdf\" target=\"_blank\">Embedded Systems<span class=\"sr-only\">, öffnet eine Datei in einem neuen Fenster</span></a></p><p>UE 066 506 <a href=\"/fileadmin/Assets/dienstleister/studienabteilung/MSc_Studienplaene_2018/MasterEnergieundAutomatisierungstechnik.pdf\" target=\"_blank\">Energie- und Automatiesierungstechnik<span class=\"sr-only\">, öffnet eine Datei in einem neuen Fenster</span></a></p><p>UE 066 507 <a href=\"/fileadmin/Assets/dienstleister/studienabteilung/MSc_Studienplaene_2019/Masterstudium_Telecommunications.pdf\" target=\"_blank\">Telecommunications <span class=\"sr-only\">, öffnet eine Datei in einem neuen Fenster</span></a>englischsprachig</p><p>UE 066 508 ";
+	// 2. parse the obtained source code (extract PDF links, descriptions, etc.)
 
 	// define the structure in which the found data will be stored (two vectors of type std::String)
 	std::vector<std::string> extract_PDF_urls;	// URLs pointing to the PDFs to be downloaded
 	std::vector<std::string> extract_url_descr;	// description of the PDF according to the '<a href =' - element
+	std::vector<int> extract_url_info;	// information about this curricula (from the two above given data entries)
 
 	// extract the data
-	testtt(result, extract_PDF_urls, extract_url_descr);
+	extract_PDF_URL_and_descriptions(result, extract_PDF_urls, extract_url_descr);
 
+	// check whether both std::vectors have the same size
+	if (extract_PDF_urls.size() != extract_url_descr.size())
+	{
+		std::cout << "error: amount of obtained data does not match (" << extract_PDF_urls.size() << " | " << extract_url_descr.size() << ")" << std::endl;
+		exit(1);
+	}
+
+	// extract additional information (BSc-, MSc-, expiring curriculum) -> iterate through each (found) element
 	for (int i = 0; i < extract_PDF_urls.size(); i++)
-		std::cout << "URL " << i << ": " << extract_PDF_urls[i] << std::endl;
+	{
+		// the elements by which the curricula are sorted by
+		std::vector<std::string> search_str {"BSc", "MSc", "Doktor", "Erweiterungsstudium", "Gemeinsame_Studienprogramme", "Alte_Studienplaene"};
 
-	for (int i = 0; i < extract_url_descr.size(); i++)
-		std::cout << "DES " << i << ": " << extract_url_descr[i] << std::endl;
+		// determine which curricula is which (of type)
+		for (int j = 0; j < search_str.size(); j++)
+		{
+			if (extract_PDF_urls[i].find(search_str[j]) != std::string::npos)
+			{
+				extract_url_info.push_back(j);
+				break;
+			}
+		}
+	}
+
+	// 3. download (all) the PDFs temporarily
 
 
+	// 4. compare PDFs (new one?, new version?, changed version? -> hash the file -> save file if it is a new one)
+
+
+	// 5. create a log of the crawl
 
 
 	// download a single PDF given by an URL
