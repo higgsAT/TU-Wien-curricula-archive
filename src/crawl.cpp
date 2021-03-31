23eff95fae5fa915df65b363fe4784cc6e9ca945
Claus Kovacs <claus@kovacs.cf>
2021-03-31 11:04:27 +0100

    moved code responsible for crawling to functions

--- src/crawl.cpp
+++ src/crawl.cpp
@@ -1,8 +1,9 @@
 #include <string>
 #include <iostream>
 #include <curl/curl.h>
+#include <fstream>
 
-// datastream (fetched source files)
+// datastream (fetched source files) used by fetch_source_single_page
 size_t write_fetched_data(void* ptr, size_t size, size_t nmemb, void* data)
 {
 	std::string* result = static_cast<std::string*>(data);
@@ -10,7 +11,38 @@ size_t write_fetched_data(void* ptr, size_t size, size_t nmemb, void* data)
 	return size*nmemb;
 }
 
-// filestream
+// fetch the source code of a single page
+std::string fetch_source_single_page(std::string url_full)
+{
+	std::string useragent = "spiderpig";		// user agent string
+
+	CURL* ch_ = curl_easy_init();				// create a CURL handle
+	char error_buffer[CURL_ERROR_SIZE];
+	std::cout << error_buffer << std::endl;		// display the error log
+
+	// SET OPTIONS
+	curl_easy_setopt(ch_, CURLOPT_ERRORBUFFER, error_buffer);			// option set for error_buffer
+	curl_easy_setopt(ch_, CURLOPT_WRITEFUNCTION, &write_fetched_data);	// pointer to the recieved data
+
+	std::string result;
+	curl_easy_setopt(ch_, CURLOPT_WRITEDATA, &result);					// write the data into this variable
+
+	int id = 1;
+	curl_easy_setopt(ch_, CURLOPT_VERBOSE, id);							// 1 ... a lot of verbose informations
+	curl_easy_setopt(ch_, CURLOPT_URL, url_full.c_str());
+	curl_easy_setopt(ch_, CURLOPT_USERAGENT, useragent.c_str());		// set user agent string
+	curl_easy_setopt(ch_, CURLOPT_CONNECTTIMEOUT, 10);					// time(seconds) we want to be connected to the server
+	curl_easy_setopt(ch_, CURLOPT_TIMEOUT, 30);							// maximum time(seconds) the transfer of the files may need
+	// SET OPTIONS
+
+	curl_easy_perform(ch_);	// start transfer with the options set above (multiple calls of this for the same handle is possible)
+	curl_easy_cleanup(ch_);	// purges the handle (when crawling is done)
+
+	return result;
+}
+
+
+// filestream used by fetch_PDF_from_URL
 size_t write_file_data(void* ptr, size_t size, size_t nmemb, FILE* stream)
 {
 	size_t written = fwrite(ptr, size, nmemb, stream);
@@ -18,11 +50,35 @@ size_t write_file_data(void* ptr, size_t size, size_t nmemb, FILE* stream)
 }
 
 
+// download a PDF given by an URL
+void fetch_PDF_from_URL(std::string fetch_pdf_url)
+{
+	CURL* curl;
+	FILE* fp;
+	CURLcode res;
+
+	char outfilename[FILENAME_MAX] = "test.pdf";
+	curl = curl_easy_init();
+
+	if (curl)
+	{
+		fp = fopen(outfilename, "wb");
+		curl_easy_setopt(curl, CURLOPT_URL, fetch_pdf_url.c_str());
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_file_data);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+		res = curl_easy_perform(curl);
+
+		// cleanup
+		curl_easy_cleanup(curl);
+		fclose(fp);
+	}
+}
+
+
 int main()
 {
 
 	// 1. fetch the source code of the page
-
 
 	// 2. extract the paths to the PDFs
 
@@ -45,30 +101,7 @@ int main()
 	/////////////////////////////////////////////
 	// fetch the source code of the given page //
 	/////////////////////////////////////////////
-	std::string url_full = "http://phys.ik.cx/programming/cpp/curl/01/";
-	std::string useragent = "spiderpig";  		// user agent string
-
-	CURL* ch_ = curl_easy_init();   			// create a CURL handle
-	char error_buffer[CURL_ERROR_SIZE];
-	std::cout << error_buffer << std::endl;		// display the error log
-
-	// SET OPTIONS
-	curl_easy_setopt(ch_, CURLOPT_ERRORBUFFER, error_buffer);			// option set for error_buffer
-	curl_easy_setopt(ch_, CURLOPT_WRITEFUNCTION, &write_fetched_data);	// pointer to the recieved data
-
-	std::string result;
-	curl_easy_setopt(ch_, CURLOPT_WRITEDATA, &result);			// write the data into this variable
-
-	int id = 1;
-	curl_easy_setopt(ch_, CURLOPT_VERBOSE, id);					// 1 ... a lot of verbose informations
-	curl_easy_setopt(ch_, CURLOPT_URL, url_full.c_str());
-	curl_easy_setopt(ch_, CURLOPT_USERAGENT, useragent.c_str());	// set user agent string
-	curl_easy_setopt(ch_, CURLOPT_CONNECTTIMEOUT, 10);				// time(seconds) we want to be connected to the server
-	curl_easy_setopt(ch_, CURLOPT_TIMEOUT, 30);					// maximum time(seconds) the transfer of the files may need
-	// SET OPTIONS
-
-	curl_easy_perform(ch_);	// start transfer with the options set above (multiple calls of this for the same handle is possible)
-	curl_easy_cleanup(ch_);	// purges the handle (when crawling is done)
+	std::string result = fetch_source_single_page("http://phys.ik.cx/programming/cpp/curl/01/");
 
 	std::cout << result << std::endl;
 	///////////////////////////////////////////
@@ -76,36 +109,11 @@ int main()
 
 
 
-
-
-
-
-
-
-
-
 	///////////////////////////////////////////
 	// download a single PDF given by an URL //
 	///////////////////////////////////////////
-	CURL* curl;
-	FILE* fp;
-	CURLcode res;
-	std::string fetch_pdf_url = "http://phys.ik.cx/physics/aufgabensammlung523.pdf";
-	char outfilename[FILENAME_MAX] = "test.pdf";
-	curl = curl_easy_init();
 
-	if (curl)
-	{
-		fp = fopen(outfilename, "wb");
-		curl_easy_setopt(curl, CURLOPT_URL, fetch_pdf_url.c_str());
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_file_data);
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-		res = curl_easy_perform(curl);
-
-		// cleanup
-		curl_easy_cleanup(curl);
-		fclose(fp);
-	}
+	fetch_PDF_from_URL("http://phys.ik.cx/physics/aufgabensammlung523.pdf");
 	///////////////////////////////////////////
 
 
