5a80454b8d2a4a2074be489d30bea06b0402c371
Claus Kovacs <claus@kovacs.cf>
2026-04-02 23:04:03 +0200

    example mail credentials

---
+++ header/mail_credentials.example.hpp
@@ -0,0 +1,14 @@
+#ifndef MAIL_CREDENTIALS_HPP
+#define MAIL_CREDENTIALS_HPP
+#include <string>
+
+/*
+ * Example credential file.
+ */
+
+inline const std::string smtp_url = "smtps://mx2e12.netcup.net:465";
+inline const std::string username  = "crawl@TLD.com";
+inline const std::string password  = "password";
+inline const std::string from      = "<crawl@TLD.com>";
+inline const std::string to        = "<crawl@TLD.com>";
+#endif
